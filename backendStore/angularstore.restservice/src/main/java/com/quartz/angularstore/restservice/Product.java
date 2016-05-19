32904ac978dcfaa543b50eaaede1c4b71025519a
Jefferson Pérez Cervera <jefferpc@gmail.com>
2016-05-19 14:06:18 -0500

    rest service project

---
+++ backendStore/angularstore.restservice/src/main/java/com/quartz/angularstore/restservice/Product.java
@@ -0,0 +1,19 @@
+package com.quartz.angularstore.restservice;
+
+public class Product {
+	private final long id;
+	private final String content;
+
+	public Product(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
